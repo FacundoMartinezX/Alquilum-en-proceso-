7558acdbf8ee6d0471abe389e1ccc2f4d1d5e303
Facumar04 <facumarrr33@gmail.com>
2025-04-28 16:58:06 -0300

    fix navbar

--- front/src/pages/Navbar.jsx
+++ front/src/pages/Navbar.jsx
@@ -4,18 +4,27 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
     return (
-        <div className="app-container">
-        <header className="navbar">
-          <nav>
-            <Link to={'/'}  className='link-reset'>
+        
+      <header className='navbar'>
+        <nav className='navbar-container'>
+          <div className='navbar-left'>
+            <Link to="/" className='link-reset'>
               <div className='logo'>Logo</div>
             </Link>
-            <ul className="navbar-list">
-              <li className="navbar-item">Coworking Spaces</li>
-              <li className="navbar-item">Perfil</li>
-            </ul>
-          </nav>
-        </header>
-      </div>
+          </div>
+
+          <div className='navbar-center'>
+              <Link to="/" className='link-reset'>
+                <div className="navbar-item">Coworking Spaces</div>
+              </Link>
+          </div>
+
+          <div className="navbar-right">
+            <Link to="/signup" className="navbar-item">SignUp</Link>
+            <Link to="/login" className="navbar-item">Login</Link>
+          </div>
+        </nav>
+
+      </header>
     );
   }
